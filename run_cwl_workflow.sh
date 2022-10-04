21b6de829ffe82eceb3dbf8f1d93d9f2f26f6b01
namratam <namrataucg@gmail.com>
2022-10-04 15:00:52 -0700

    removed science code clone

--- run_cwl_workflow.sh
+++ run_cwl_workflow.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cwl_workflow_file_path = $0
+cwl_workflow_file_url = $0
 
 # Create yaml file for the run
 python create_cwl_yml.py
 
 # run cwl workflow
 # Question: what role does the /tmp dir play?
-cwltool --no-read-only --outdir /tmp /some/location/$cwl_workflow_file_path workflow_yaml.yml
+cwltool --no-read-only --outdir /tmp $cwl_workflow_file_url workflow_yaml.yml
