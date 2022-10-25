dff9bef2ed75764d5cfee229a78c30c5330cacfe
Drew Meyers <drew.meyers@jpl.nasa.gov>
2022-10-25 12:50:46 -0700

    chore: Fixes to the Dockerfile and run script

--- run_cwl_workflow.sh
+++ run_cwl_workflow.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cwl_workflow_file_url = $0
+cwl_workflow_file_url=$0
 
 # Create yaml file for the run
-python create_cwl_yml.py
+python3 create_cwl_yml.py
 
 # run cwl workflow
 # Question: what role does the /tmp dir play?
-cwltool --no-read-only --outdir /tmp $cwl_workflow_file_url workflow_yaml.yml
+/home/ops/verdi/bin/cwl-runner --no-read-only --outdir /tmp $cwl_workflow_file_url workflow_yaml.yml
