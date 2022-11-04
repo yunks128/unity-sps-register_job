39e7c81a8107a746859f323b5ee34eb861d91b6f
Drew Meyers <drew.meyers@jpl.nasa.gov>
2022-11-03 17:01:35 -0700

    feat: update path to cwl scripts in docker

--- run_cwl_workflow.sh
+++ run_cwl_workflow.sh
@@ -3,7 +3,7 @@
 cwl_workflow_file_url=$0
 
 # Create yaml file for the run
-python3 create_cwl_yml.py
+python3 /cwl/create_cwl_yml.py
 
 # run cwl workflow
 # Question: what role does the /tmp dir play?
