14cb36a4dee177c7e5253bcc71e1fd911088d434
Ryan Hunter <ryan.g.hunter@jpl.nasa.gov>
2023-04-28 16:03:48 -0700

    change job publishing script and docker image to support new cwl

--- run_cwl_workflow.sh
+++ run_cwl_workflow.sh
@@ -4,12 +4,10 @@ cwl_workflow_file_url=$1
 
 # Create yaml files for the run
 /home/ops/verdi/bin/python /cwl/create_cwl_yml.py
-/home/ops/verdi/bin/python /cwl/create_job_data.py --status running
 
 # run cwl workflow
 # Question: what role does the /tmp dir play?
-/home/ops/verdi/bin/cwl-runner --no-read-only --outdir /tmp /cwl/publish_job_status.cwl publish_job_status.yml
+/home/ops/verdi/bin/cwl-runner --no-read-only --outdir /tmp /cwl/publish_job_status.cwl workflow_yaml.yml
 /home/ops/verdi/bin/cwl-runner --no-read-only --outdir /tmp $cwl_workflow_file_url workflow_yaml.yml > cwl_outputs.txt
 # TODO: use status code of cwl runner to determine success/fail
-/home/ops/verdi/bin/python /cwl/create_job_data.py --status succeeded --outputs_file cwl_outputs.txt
-/home/ops/verdi/bin/cwl-runner --no-read-only --outdir /tmp /cwl/publish_job_status.cwl publish_job_status.yml
+/home/ops/verdi/bin/cwl-runner --no-read-only --outdir /tmp /cwl/publish_job_status.cwl workflow_yaml.yml
