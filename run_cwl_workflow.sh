03352bcd14ade7545929a25d1c8bb202bfaa307b
Ryan Hunter <ryan.g.hunter@jpl.nasa.gov>
2023-05-01 16:14:25 -0700

    clean up files after input switch for job notifications

--- run_cwl_workflow.sh
+++ run_cwl_workflow.sh
@@ -2,12 +2,12 @@
 
 cwl_workflow_file_url=$1
 
-# Create yaml files for the run
-/home/ops/verdi/bin/python /cwl/create_cwl_yml.py
+# Create yaml file for the run
+python3 /cwl/create_cwl_yml.py
 
 # run cwl workflow
 # Question: what role does the /tmp dir play?
 /home/ops/verdi/bin/cwl-runner --no-read-only --outdir /tmp /cwl/publish_job_status.cwl workflow_yaml.yml
-/home/ops/verdi/bin/cwl-runner --no-read-only --outdir /tmp $cwl_workflow_file_url workflow_yaml.yml > cwl_outputs.txt
+/home/ops/verdi/bin/cwl-runner --no-read-only --outdir /tmp $cwl_workflow_file_url workflow_yaml.yml
 # TODO: use status code of cwl runner to determine success/fail
 /home/ops/verdi/bin/cwl-runner --no-read-only --outdir /tmp /cwl/publish_job_status.cwl workflow_yaml.yml
